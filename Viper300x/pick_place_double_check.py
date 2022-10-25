4f0053bb400bf1577d6a382b61d1707266e4263c
WheelyNinja <74386875+WheelyNinja@users.noreply.github.com>
2022-10-25 14:38:58 -0600

    Update pick_place_double_check.py

--- Viper300x/pick_place_double_check.py
+++ Viper300x/pick_place_double_check.py
@@ -12,45 +12,48 @@ from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterfa
 # Then change to this directory and type 'python pick_place.py'
 
 def main():
-    # Initialize the arm module along with the pointcloud and armtag modules
-    bot = InterbotixManipulatorXS("vx300s", moving_time=1.5, accel_time=.75)
-    pcl = InterbotixPointCloudInterface()
-    armtag = InterbotixArmTagInterface()
+	# Initialize the arm module along with the pointcloud and armtag modules
+	bot = InterbotixManipulatorXS("vx300s", moving_time=1.5, accel_time=.75)
+	pcl = InterbotixPointCloudInterface()
+	armtag = InterbotixArmTagInterface()
 
-    # set initial arm and gripper pose
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.gripper.open()
+	# set initial arm and gripper pose
+	bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+	bot.gripper.open()
 
-    # get the ArmTag pose
-    # bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
-    # time.sleep(0.5)
-    # armtag.find_ref_to_arm_base_transform()
-    #bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+	# get the ArmTag pose
+	# bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
+	# time.sleep(0.5)
+	# armtag.find_ref_to_arm_base_transform()
+	#bot.arm.set_ee_pose_components(x=0.3, z=0.2)
 
-    # get the cluster positions
-    # sort them from max to min 'x' position w.r.t. the 'wx200/base_link' frame
-    success, clusters = pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
+	# get the cluster positions
+	# sort them from max to min 'x' position w.r.t. the 'wx200/base_link' frame
+	success, clusters = pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
 
-    # pick up all the objects and drop them in a virtual basket in front of the robot
-    if success == 1:
-	    for cluster in clusters:
-            #r, g, b = cluster["color"]
-            x, y, z = cluster["position"]
-            #print(r,g,b)
-            bot.arm.set_ee_pose_components(x=x+.025, y=y-0.03, z=z+0.065, pitch=0.5)
-            bot.arm.set_ee_pose_components(x=x+.025, y=y-0.03, z=z+0.025, pitch=0.5)
-            bot.gripper.close()
-            bot.arm.set_ee_pose_components(x=x+.02, y=y-0.03, z=z+0.1, pitch=0.5)
-            bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-                
-            bot.arm.set_ee_pose_components(y=-0.35, z=0.2)
-            bot.arm.set_ee_pose_components(y=-0.4, z=0.1)
-            bot.gripper.open() 
-            bot.arm.set_ee_pose_components(y=-0.35, z=0.2)
-            bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-            success, clusters = pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
-    else:
-        bot.arm.go_to_sleep_pose()
-	    
+	# pick up all the objects and drop them in a virtual basket in front of the robot
+	
+	# cluster might need length of instead
+	for cluster in clusters:
+		#r, g, b = cluster["color"]
+		if success == 1:
+			x, y, z = cluster["position"]
+			#print(r,g,b)
+			bot.arm.set_ee_pose_components(x=x+.025, y=y-0.03, z=z+0.065, pitch=0.5)
+			bot.arm.set_ee_pose_components(x=x+.025, y=y-0.03, z=z+0.025, pitch=0.5)
+			bot.gripper.close()
+			bot.arm.set_ee_pose_components(x=x+.02, y=y-0.03, z=z+0.1, pitch=0.5)
+			bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+
+			bot.arm.set_ee_pose_components(y=-0.35, z=0.2)
+			bot.arm.set_ee_pose_components(y=-0.4, z=0.1)
+			bot.gripper.open() 
+			bot.arm.set_ee_pose_components(y=-0.35, z=0.2)
+			bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+			success, clusters = pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
+		else:
+			bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+			bot.arm.go_to_sleep_pose()
+
 if __name__=='__main__':
-    main()
+	main()
