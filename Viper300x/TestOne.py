8f9fd544109bde3a656902ca396c5f269b1659d5
rodmigue <80237195+rodmigue@users.noreply.github.com>
2023-03-06 16:55:46 -0700

    Add files via upload

--- Viper300x/TestOne.py
+++ Viper300x/TestOne.py
@@ -1,6 +1,9 @@
-from interbotix_xs_modules.arm import InterbotixManipulatorXS
-import numpy as np
 import time as tm
+import numpy as np
+from interbotix_xs_modules.arm import InterbotixManipulatorXS
+from interbotix_perception_modules.armtag import InterbotixArmTagInterface
+from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
+
 
 # This script makes the end-effector perform pick, pour, and place tasks
 #
@@ -8,15 +11,30 @@ import time as tm
 # Then change to this directory and type 'python bartender.py'
 
 def main():
-    bot = InterbotixManipulatorXS("vx300s", "arm", "gripper")
-    bot.arm.set_ee_pose_components(x=0.3, y=0.5,z=0.3)
-    # bot.arm.set_single_joint_position("waist", np.pi/2.0)
-    bot.gripper.open()
-    tm.sleep(1)
-    bot.gripper.close()
-    bot.arm.go_to_home_pose()
-    bot.arm.go_to_sleep_pose()
- 
+	bot = InterbotixManipulatorXS("vx300s", moving_time=2.5, accel_time=.75)
+	pcl = InterbotixPointCloudInterface()
+	armtag = InterbotixArmTagInterface()
+	#print(x,y,z)
+	bot.arm.go_to_home_pose()
+	bot.arm.set_ee_pose_components(x=.3, z=0.2, pitch = np.pi/4)
+	#bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+	#pos = bot.arm.get_joint_commands()
+	#print(pos)
+	#pos[4] += np.pi/2.0
+	#print(pos)
+	#bot.arm.set_joint_positions(pos)
+	#bot.arm.set_ee_cartesian_trajectory(z=-0.15)
+	#bot.arm.set_ee_pose_components(y=0.3, z=0.2)
+	#bot.arm.set_single_joint_position("waist", np.pi/2.0)
+	#bot.arm.set_ee_cartesian_trajectory(yaw=np.pi/2.0)
+	bot.gripper.open()
+	tm.sleep(30)
+	armtag.find_ref_to_arm_base_transform()
+	tm.sleep(999)
+	bot.gripper.close()
+	bot.arm.go_to_home_pose()
+	bot.arm.go_to_sleep_pose()
+
 
 if __name__=='__main__':
-    main()
+	main()
